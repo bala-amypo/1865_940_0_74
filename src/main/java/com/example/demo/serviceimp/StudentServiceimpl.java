8ffaaa33e97243740dd25a0890861b7bb7761557
bala-amypo <bala@amypo.in>
2025-12-17 09:35:31 +0000

    User commit

--- src/main/java/com/example/demo/serviceimp/StudentServiceimpl.java
+++ src/main/java/com/example/demo/serviceimp/StudentServiceimpl.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 import org.springframework.stereotypes.service;
+
 import com.example.demo.service.StudentService;
 @service
 public class StudentServiceimpl implements Student{
+    public Student saveStudent(Student student)
+    {
 
+    }
 
 }
