7b30b6348460934168f281f7435d1d72c62de06b
bala-amypo <bala@amypo.in>
2025-12-18 04:30:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,14 +1,12 @@
 package com.example.demo.entity;
-
 import java.time.LocalDate;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
+import jakarta.persistence.Table;
 @Entity
-//@Table(name="StudentTable")
+ @Table(name="StudentTable")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,10 +31,13 @@ public class Student {
     public void setEmail(String email) {
         this.email = email;
     }
-    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
+   
+    
+    public Student(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
+        
     }
     public Student() {
     } 
--- src/main/java/com/example/demo/serviceimp/StudentServiceimpl.java
+++ src/main/java/com/example/demo/serviceimp/StudentServiceimpl.java
@@ -1,21 +1,20 @@
-package com.example.demo.service;
-import org.springframework.stereotypes.service;
+
+package com.example.demo.Serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
-@service
-public class StudentServiceimpl implements Student{
-    // public Student saveStudent(Student student)
-    // {
-    //     private final StudentRepository studentrepository;
-    //     public StudentServiceimpl(StudentRepository studentrepository){
-    //         this.StudentRepository=studentrepository;
-    //     }
-    // }
+
+@Service
+public class StudentServiceImple implements StudentService {
     @Autowired
-    StudentRepository studentrepository;
+    StudentRepository studentRepository;
 
     public Student saveStudent(Student student){
-        return studentrepository.save(student);
+        return studentRepository.save(student);
     }
-
+    
 }
